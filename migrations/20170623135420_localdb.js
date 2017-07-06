cbafc97fa78e7aa2b3ac10a5b84ba943a214d5aa
Manpreet Matharu <manpreetmatharu@amp17-17.leoguest.fg.rbc.com>
2017-07-06 10:31:55 -0400

    modified knex db schema

--- migrations/20170623135420_localdb.js
+++ migrations/20170623135420_localdb.js
@@ -1,4 +1,4 @@
-import Knex from 'knex'
+import Knex from "knex";
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable("user", function(table) {
@@ -16,8 +16,15 @@ exports.up = function(knex, Promise) {
       table.increments("id").primary();
       table.text("title").notNullable();
       table.text("body").notNullable();
-      table.integer('score').notNullable().defaultTo(0)
-      table.float('hotScore').notNullable().defaultTo(0)
+      table.integer("score").notNullable().defaultTo(0);
+      table.float("hotScore").notNullable().defaultTo(0);
+      table.integer("postedBy").references("id").inTable("user");
+      table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+    }),
+
+    knex.schema.createTable("question", function(table) {
+      table.increments("id").primary();
+      table.text("title").notNullable();
       table.integer("postedBy").references("id").inTable("user");
       table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
     }),
@@ -29,22 +36,30 @@ exports.up = function(knex, Promise) {
       table.integer("postId").references("id").inTable("post");
       table.dateTime("createdAt");
     }),
+    knex.schema.createTable("answer", function(table) {
+      table.increments("id").primary();
+      table.text("content").notNullable();
+      table.integer("postedBy").references("id").inTable("user");
+      table.integer("quesId").references("id").inTable("question");
+      table.dateTime("createdAt");
+    }),
 
     knex.schema.createTable("vote", function(table) {
       table.increments("id").primary();
-      table.integer("vote_value").notNullable();    
-      table.integer("votedBy").references("id").inTable("user");    
+      table.integer("vote_value").notNullable();
+      table.integer("votedBy").references("id").inTable("user");
       table.integer("postId").unique().references("id").inTable("post");
- 
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
+    knex.schema.dropTable("vote"),
+    knex.schema.dropTable("comment"),
     knex.schema.dropTable("user"),
     knex.schema.dropTable("post"),
-    knex.schema.dropTable("comment"),
-    knex.schema.dropTable("vote")
+    knex.schema.dropTable("answer"),
+    knex.schema.dropTable("question"),
   ]);
 };
